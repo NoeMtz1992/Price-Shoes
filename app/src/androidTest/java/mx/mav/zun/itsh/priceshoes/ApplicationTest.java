fb8ae99c909c45a57e84608ae8aea2f700c6f338
NoeMtz1992 <noemtz1992@hotmail.com>
2015-04-03 14:28:00 -0600

    Initial commit

---
+++ app/src/androidTest/java/mx/mav/zun/itsh/priceshoes/ApplicationTest.java
@@ -0,0 +1,13 @@
+package mx.mav.zun.itsh.priceshoes;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
